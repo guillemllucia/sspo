da1a239167f814a2839e153a3044afa145810abf
guillemllucia <guillemllucia@gmail.com>
2025-08-27 16:24:52 +0200

    added tqdm to requirements.txt

--- sspo/api/fast.py
+++ sspo/api/fast.py
@@ -1,1 +1,9 @@
-app= xgb_reg.pkl
+import pickle
+from fastapi import FastAPI
+
+app = FastAPI()
+xgb_reg = pickle.load(open('xgb_reg.pkl', 'rb'))
+
+@app.get('/')
+def index():
+    return {'ok': True}
